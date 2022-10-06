6ffa66136d7f67ad11e2420d402e1f0af32e892f
Jam-Burger <jay40793@gmail.com>
2022-10-07 01:53:59 +0530

    complete tic-tac-toe game

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -14,9 +14,7 @@ const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.getElementById("board");
 const winningMessageElement = document.getElementById("winningMessage");
 const restartButton = document.getElementById("restartButton");
-const winningMessageTextElement = document.querySelector(
-  "[data-winning-message-text]"
-);
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
 let circleTurn;
 
 startGame();
