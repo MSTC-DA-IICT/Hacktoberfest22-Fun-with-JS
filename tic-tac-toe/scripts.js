987437b89b4c52e2166e56c2487f2b5dc609abf1
Jam-Burger <jay40793@gmail.com>
2022-10-06 23:59:01 +0530

    renamed variable CIRCLE_CLASS to O_CLASS

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -1,5 +1,5 @@
-const X_CLASS = "x";
-const CIRCLE_CLASS = "circle";
+const X_CLASS = "X";
+const O_CLASS = "O";
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -27,7 +27,7 @@ function startGame() {
   circleTurn = false;
   cellElements.forEach((cell) => {
     cell.classList.remove(X_CLASS);
-    cell.classList.remove(CIRCLE_CLASS);
+    cell.classList.remove(O_CLASS);
     cell.removeEventListener("click", handleClick);
     cell.addEventListener("click", handleClick, { once: true });
   });
@@ -37,7 +37,7 @@ function startGame() {
 
 function handleClick(e) {
   const cell = e.target;
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  const currentClass = circleTurn ? O_CLASS : X_CLASS;
   placeMark(cell, currentClass);
   if (checkWin(currentClass)) {
     endGame(false);
@@ -61,7 +61,7 @@ function endGame(draw) {
 function isDraw() {
   return [...cellElements].every((cell) => {
     return (
-      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+      cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS)
     );
   });
 }
@@ -76,9 +76,9 @@ function swapTurns() {
 
 function setBoardHoverClass() {
   board.classList.remove(X_CLASS);
-  board.classList.remove(CIRCLE_CLASS);
+  board.classList.remove(O_CLASS);
   if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS);
+    board.classList.add(O_CLASS);
   } else {
     board.classList.add(X_CLASS);
   }
